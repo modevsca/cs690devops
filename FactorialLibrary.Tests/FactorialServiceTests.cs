b62a27b8b1c34745360b749c3360c9b58937fd59
modevsca <modevsca@gmail.com>
2026-07-05 21:38:02 +0000

    negative number implemented

--- FactorialLibrary.Tests/FactorialServiceTests.cs
+++ FactorialLibrary.Tests/FactorialServiceTests.cs
@@ -1,12 +1,27 @@
-﻿namespace FactorialService.Tests;
-using FactorialLibrary;
-public class FactorialServiceTests
+﻿namespace FactorialLibrary;
+public class FactorialService
 {
-    [Fact]
-    public void Test1()
+    public static int Factorial(int n)
     {
-        var result = FactorialService.Factorial(0);
-        Assert.Equal(1,result);
+        if (n < 0)
+        {
+            throw new InvalidOperationException("Negative numbers don't have proper factorial");
 
+        }
+        if(n==1 || n == 0)
+        {
+            return 1;
+
+        }
+        return n*Factorial(n-1);
+    }
+    [Fact]
+    public void Test_negative()
+    {
+        Assert.Throws<InvalidOperationException>(() =>
+        {
+            var result =  FactorialService.Factorial(-1);
+
+        });
     }
 }
